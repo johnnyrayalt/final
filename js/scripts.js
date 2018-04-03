598ff716d0fdfe4c4b5b03ee3ec6b39d135a58a7
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-04-03 00:00:17 -0700

    add second hand functunality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
+
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds/60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(seconds);
+}
+setInterval(setDate, 1000);
+
+setDate();
+
+
 $(document).ready(function() {
   $('.add-clock form').submit(function(event) {
     event.preventDefault();
 
     var city = $('#city').val();
-    $('.city-name').text(city);
-
-    $('.each-clock').show();
 
 
     $('.each-clock').append('<p>' + '<span class="city-name"></span>' + '</p>' + '<div class="clock">' + '<div class="face">' + '<div class="hand second-hand">' + '</div' + '<div class="hand minute-hand">' + '</div>' + '<div class="hand hour-hand">' + '<div>' + '</div>' + '</div>');
 
-
+$('.city-name').text(city);
   });
 });
